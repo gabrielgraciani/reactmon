3864c6215718da71e44106aded94f8482c576e6f
Gabriel Graciani <gabrieltgraciani@hotmail.com>
2020-01-14 22:05:41 -0300

    Começo Header

--- src/components/header.js
+++ src/components/header.js
@@ -3,8 +3,32 @@ import React, {Component} from 'react';
 class Header extends Component{
 	render(){
 		return(
-			<div>
-				Header
+			<div id="wrap_header">
+				<div className="indent">
+					<div className="logo">
+						<h2>Reactmon</h2>
+					</div>
+
+					<div className="hamburguer">
+						<div className="risco"></div>
+						<div className="risco"></div>
+						<div className="risco"></div>
+					</div>
+
+					<div className="menu active">
+						<div className="fechar">
+							<div className="risco risco1"></div>
+							<div className="risco risco2"></div>
+						</div>
+						<ul>
+							<li>Home</li>
+							<li>Pokedéx</li>
+							<li>Itens</li>
+							<li>Cidades</li>
+						</ul>
+					</div>
+
+				</div>
 			</div>
 		)
 	}
