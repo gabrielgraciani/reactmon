b28e5abf44ba84f32a05772d564699135c959c4d
Gabriel Thomaz Graciani <gabrieltgraciani@hotmail.com>
2020-01-15 12:49:53 -0300

    troca de state por useState no header

--- src/components/header.js
+++ src/components/header.js
@@ -1,56 +1,50 @@
-import React, {Component} from 'react';
+import React, {useState} from 'react';
 import Link from 'next/link';
 
-class Header extends Component{
-	constructor(props) {
-		super(props);
-		this.state = {isOpen: false};
-		this.changeSkin = this.changeSkin.bind(this);
-	}
+function Header(){
+	const [isOpen, setIsOpen] = useState(false);
+	const change = () => {
+		const teste = !isOpen;
+		setIsOpen(teste);
+	};
 
-	changeSkin() {
-		this.setState({isOpen: !this.state.isOpen});
-	}
+	return(
+		<div id="wrap_header">
+			<div className="indent">
+				<div className="logo">
+					<h2>Reactmon</h2>
+				</div>
 
-	render(){
-		return(
-			<div id="wrap_header">
-				<div className="indent">
-					<div className="logo">
-						<h2>Reactmon</h2>
-					</div>
+				<div className={`hamburguer ${isOpen ? 'active' : ''}`} onClick={change}>
+					<div className="risco risco1"></div>
+					<div className="risco risco2"></div>
+					<div className="risco risco3"></div>
+				</div>
 
-					<div className={`hamburguer ${this.state.isOpen ? 'active' : ''}`} onClick={this.changeSkin}>
+				<div className={`menu ${isOpen ? 'active' : ''}`}>
+					<div className={`fechar ${isOpen ? 'active' : ''}`}  onClick={change}>
 						<div className="risco risco1"></div>
 						<div className="risco risco2"></div>
-						<div className="risco risco3"></div>
 					</div>
-
-					<div className={`menu ${this.state.isOpen ? 'active' : ''}`}>
-						<div className={`fechar ${this.state.isOpen ? 'active' : ''}`}  onClick={this.changeSkin}>
-							<div className="risco risco1"></div>
-							<div className="risco risco2"></div>
-						</div>
-						<ul>
-							<Link href="/">
-								<li onClick={this.changeSkin}>Home</li>
-							</Link>
-							<Link href="/pokedex">
-								<li onClick={this.changeSkin}>Pokedéx</li>
-							</Link>
-							<Link href="/itens">
-								<li onClick={this.changeSkin}>Itens</li>
-							</Link>
-							<Link href="/cidades">
-								<li onClick={this.changeSkin}>Cidades</li>
-							</Link>
-						</ul>
-					</div>
-
+					<ul>
+						<Link href="/">
+							<li onClick={change}>Home</li>
+						</Link>
+						<Link href="/pokedex">
+							<li onClick={change}>Pokedéx</li>
+						</Link>
+						<Link href="/itens">
+							<li onClick={change}>Itens</li>
+						</Link>
+						<Link href="/cidades">
+							<li onClick={change}>Cidades</li>
+						</Link>
+					</ul>
 				</div>
+
 			</div>
-		)
-	}
+		</div>
+	)
 }
 
 export default Header;
