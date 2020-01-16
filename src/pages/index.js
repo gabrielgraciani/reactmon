256ca6012545cf8173a22d2d34261496fd05824b
Gabriel Graciani <gabrieltgraciani@hotmail.com>
2020-01-15 22:15:21 -0300

    começo tela inicial

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,11 +1,14 @@
 import React from 'react'
+import Background from '../assets/images/bg-item.png';
+import Pokemon from '../assets/images/venusaur.png';
+
 
 const Home = () => (
   <div id="wrap_pokemons">
 	  <div className="indent">
 		  <div className="item">
-			  <img src={`${require("../assets/images/bg-item.png")}`} className="fundo" />
-			  <img src={`${require("../assets/images/venusaur.png")}`} className="pokemon" />
+			  <img src={`${Background}`} className="fundo" />
+			  <img src={`${Pokemon}`} className="pokemon" />
 			  <div className="nome">
 				  <h2>Venusaur</h2>
 			  </div>
@@ -18,10 +21,9 @@ const Home = () => (
 				  </div>
 			  </div>
 		  </div>
-
 		  <div className="item">
-			  <img src={`${require("../assets/images/bg-item.png")}`} className="fundo" />
-			  <img src={`${require("../assets/images/venusaur.png")}`} className="pokemon" />
+			  <img src={`${Background}`} className="fundo" />
+			  <img src={`${Pokemon}`} className="pokemon" />
 			  <div className="nome">
 				  <h2>Venusaur</h2>
 			  </div>
@@ -34,10 +36,9 @@ const Home = () => (
 				  </div>
 			  </div>
 		  </div>
-
 		  <div className="item">
-			  <img src={`${require("../assets/images/bg-item.png")}`} className="fundo" />
-			  <img src={`${require("../assets/images/venusaur.png")}`} className="pokemon" />
+			  <img src={`${Background}`} className="fundo" />
+			  <img src={`${Pokemon}`} className="pokemon" />
 			  <div className="nome">
 				  <h2>Venusaur</h2>
 			  </div>
@@ -50,10 +51,39 @@ const Home = () => (
 				  </div>
 			  </div>
 		  </div>
-
 		  <div className="item">
-			  <img src={`${require("../assets/images/bg-item.png")}`} className="fundo" />
-			  <img src={`${require("../assets/images/venusaur.png")}`} className="pokemon" />
+			  <img src={`${Background}`} className="fundo" />
+			  <img src={`${Pokemon}`} className="pokemon" />
+			  <div className="nome">
+				  <h2>Venusaur</h2>
+			  </div>
+			  <div className="tipo">
+				  <div className="tip">
+					  <h4>Grass</h4>
+				  </div>
+				  <div className="tip">
+					  <h4>Poison</h4>
+				  </div>
+			  </div>
+		  </div>
+		  <div className="item">
+			  <img src={`${Background}`} className="fundo" />
+			  <img src={`${Pokemon}`} className="pokemon" />
+			  <div className="nome">
+				  <h2>Venusaur</h2>
+			  </div>
+			  <div className="tipo">
+				  <div className="tip">
+					  <h4>Grass</h4>
+				  </div>
+				  <div className="tip">
+					  <h4>Poison</h4>
+				  </div>
+			  </div>
+		  </div>
+		  <div className="item">
+			  <img src={`${Background}`} className="fundo" />
+			  <img src={`${Pokemon}`} className="pokemon" />
 			  <div className="nome">
 				  <h2>Venusaur</h2>
 			  </div>
