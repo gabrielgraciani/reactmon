c984a961e144abc818c05406d77b18c6830d76d0
Gabriel Graciani <gabrieltgraciani@hotmail.com>
2020-01-19 20:14:38 -0300

    pokemons vindo do json

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,112 +1,61 @@
-import React from 'react'
+import React, {useState, useEffect} from 'react'
+import axios from 'axios';
 import Background from '../assets/images/bg-item.png';
 import Pokemon from '../assets/images/venusaur.png';
 import { Carousel } from "react-responsive-carousel";
 import styles from "react-responsive-carousel/lib/styles/carousel.min.css";
 
-const Home = () => (
-	<>
-	<div id="wrap_banner">
-		<Carousel showThumbs={false} infiniteLoop={true} showStatus={false} autoplay={true} interval={3000}>
-			<div className="item">this is slide 1</div>
-			<div className="item">this is slide 2</div>
-			<div className="item">this is slide 3</div>
-		</Carousel>
-	</div>
-  <div id="wrap_pokemons">
-	  <div className="indent">
-		  <div className="item">
-			  <img src={`${Background}`} className="fundo" />
-			  <div className="imagem">
-			 	<img src={`${Pokemon}`} className="pokemon" />
-			  </div>
-			  <div className="nome">
-				  <h2>Venusaur</h2>
-			  </div>
-			  <div className="tipo">
-				  <div className="tip grass">
-					  <h4>Grass</h4>
-				  </div>
-				  <div className="tip poison">
-					  <h4>Poison</h4>
-				  </div>
-			  </div>
-		  </div>
+function Home(){
+	const [data, setData] = useState({ pokemon: [] });
+	useEffect(() => {
+		const fetchData = async () => {
+			const result = await axios(
+				'http://localhost:3000/api/v1/pokemon',
+			);
+			console.log(result);
+			setData(result.data);
+		};
+		fetchData();
+	}, []);
+	return(
+		<>
+		<div id="wrap_banner">
+			<Carousel showThumbs={false} infiniteLoop={true} showStatus={false} autoplay={true} interval={3000}>
+				<div className="item">this is slide 1</div>
+				<div className="item">this is slide 2</div>
+				<div className="item">this is slide 3</div>
+			</Carousel>
+		</div>
 
-		  <div className="item">
-			  <img src={`${Background}`} className="fundo" />
-			  <div className="imagem">
-				  <img src={`${Pokemon}`} className="pokemon" />
-			  </div>
-			  <div className="nome">
-				  <h2>Venusaur</h2>
-			  </div>
-			  <div className="tipo">
-				  <div className="tip fire">
-					  <h4>Fire</h4>
-				  </div>
-				  <div className="tip water">
-					  <h4>Water</h4>
-				  </div>
-			  </div>
-		  </div>
+		<ul>
 
-		  <div className="item">
-			  <img src={`${Background}`} className="fundo" />
-			  <div className="imagem">
-				  <img src={`${Pokemon}`} className="pokemon" />
-			  </div>
-			  <div className="nome">
-				  <h2>Venusaur</h2>
-			  </div>
-			  <div className="tipo">
-				  <div className="tip eletric">
-					  <h4>Eletric</h4>
-				  </div>
-				  <div className="tip bug">
-					  <h4>Bug</h4>
-				  </div>
-			  </div>
-		  </div>
+		</ul>
 
-		  <div className="item">
-			  <img src={`${Background}`} className="fundo" />
-			  <div className="imagem">
-				  <img src={`${Pokemon}`} className="pokemon" />
-			  </div>
-			  <div className="nome">
-				  <h2>Venusaur</h2>
-			  </div>
-			  <div className="tipo">
-				  <div className="tip flying">
-					  <h4>Flying</h4>
-				  </div>
-				  <div className="tip normal">
-					  <h4>Normal</h4>
-				  </div>
-			  </div>
-		  </div>
+		<div id="wrap_pokemons">
+			<div className="indent">
+				{data.pokemon.map(item => (
+					<div className="item" key={item.id}>
+						<img src={`${Background}`} className="fundo" />
+						<div className="imagem">
+							<img src={item.img} className="pokemon" />
+						</div>
+						<div className="nome">
+							<h2>{item.name}</h2>
+						</div>
+						<div className="tipo">
+							{item.type.map(teste => (
+								<div className={`tip ${teste.toLowerCase()}`}>
+									<h4>{teste}</h4>
+								</div>
+							))}
+						</div>
+					</div>
+				))}
+			</div>
+		</div>
+		</>
+	)
 
-		  <div className="item">
-			  <img src={`${Background}`} className="fundo" />
-			  <div className="imagem">
-				  <img src={`${Pokemon}`} className="pokemon" />
-			  </div>
-			  <div className="nome">
-				  <h2>Venusaur</h2>
-			  </div>
-			  <div className="tipo">
-				  <div className="tip ground">
-					  <h4>Ground</h4>
-				  </div>
-				  <div className="tip fairy">
-					  <h4>Fairy</h4>
-				  </div>
-			  </div>
-		  </div>
-	  </div>
-  </div>
-	</>
-);
+}
 
 export default Home
