d46885d15e2a2c28ebaaabf22665686ee15bea17
Gabriel Graciani <gabrieltgraciani@hotmail.com>
2020-01-20 22:01:53 -0300

    isolando o componente firebase

--- src/pages/Login.js
+++ src/pages/Login.js
@@ -1,19 +1,6 @@
 import React, {useState, useEffect} from 'react';
-import firebase from 'firebase/app';
-import 'firebase/auth';
+import firebase from '../services/firebase';
 
-const config = {
-	apiKey: "AIzaSyD3blW99oykENBSWnwjch4xYXWWuZf2IVU",
-	authDomain: "reactmon-55f1b.firebaseapp.com",
-	databaseURL: "https://reactmon-55f1b.firebaseio.com",
-	projectId: "reactmon-55f1b",
-	storageBucket: "reactmon-55f1b.appspot.com",
-	messagingSenderId: "172940760428",
-	appId: "1:172940760428:web:6f97ef21ed4d6b63e28f49",
-	measurementId: "G-L487SHSNG1"
-};
-// Initialize Firebase
-firebase.initializeApp(config);
 
 const login = () => {
 	const provider = new firebase.auth.GithubAuthProvider();
---
+++ src/services/firebase.js
@@ -0,0 +1,17 @@
+import firebase from 'firebase/app';
+import 'firebase/auth';
+
+const config = {
+	apiKey: "AIzaSyD3blW99oykENBSWnwjch4xYXWWuZf2IVU",
+	authDomain: "reactmon-55f1b.firebaseapp.com",
+	databaseURL: "https://reactmon-55f1b.firebaseio.com",
+	projectId: "reactmon-55f1b",
+	storageBucket: "reactmon-55f1b.appspot.com",
+	messagingSenderId: "172940760428",
+	appId: "1:172940760428:web:6f97ef21ed4d6b63e28f49",
+	measurementId: "G-L487SHSNG1"
+};
+// Initialize Firebase
+firebase.initializeApp(config);
+
+export default firebase;
