13ecc157134b5da8795184ce841797c53b4132c2
Gabriel Thomaz Graciani <gabrieltgraciani@hotmail.com>
2020-01-22 10:37:29 -0300

    fazendo o checkbox

--- src/pages/Login.js
+++ src/pages/Login.js
@@ -7,6 +7,14 @@ const login = () => {
 	firebase.auth().signInWithRedirect(provider)
 };
 
+const Checkbox = ({ type = "checkbox", name, checked = false, onChange }) => {
+	console.log("Checkbox: ", name, checked);
+
+	return (
+		<input type={type} name={name} checked={checked} onChange={onChange} />
+	);
+};
+
 function Login () {
 	const [userInfo, setUserInfo] = useState({
 		isUserLoggedIn: false,
@@ -93,6 +101,30 @@ function Login () {
 
 
 
+	const [checkedItems, setCheckedItems] = useState({});
+
+	const handleChangeBox = event => {
+		setCheckedItems({
+			...checkedItems,
+			[event.target.name]: event.target.checked
+		});
+		console.log("checkedItems: ", checkedItems);
+	};
+
+	const checkboxes = [
+		{
+			name: "check-box-1",
+			key: "checkBox1",
+			label: "Check Box 1"
+		},
+		{
+			name: "check-box-2",
+			key: "checkBox2",
+			label: "Check Box 2"
+		}
+	];
+
+
 	return(
 		<div>
 			{isUserLoggedIn && (
@@ -127,7 +159,17 @@ function Login () {
 							<input type="text" name="imagem" value={values.imagem} onChange={handleChange} placeholder="Imagem" />
 						</div>
 						<div className="item">
-							<input type="text" name="tipo" value={values.tipo} onChange={handleChange} placeholder="Tipo" />
+							tipo pokemon:
+							{checkboxes.map(item => (
+								<label key={item.key}>
+									{item.name}
+									<Checkbox
+										name={item.name}
+										checked={checkedItems[item.name]}
+										onChange={handleChangeBox}
+									/>
+								</label>
+							))}
 						</div>
 						<div className="item">
 							<input type="text" name="altura" value={values.altura} onChange={handleChange} placeholder="Altura" />
