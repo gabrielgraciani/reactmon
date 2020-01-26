de737378f53b0e526f32dd6a4d602fe9e72d539e
Gabriel Graciani <gabrieltgraciani@hotmail.com>
2020-01-25 22:47:01 -0300

    feito tela de login

--- frontend/src/pages/login.js
+++ frontend/src/pages/login.js
@@ -1,19 +1,23 @@
 import React, {useContext} from 'react';
 import PokemonList from 'components/login/pokemonList';
 import {AuthContext} from 'contexts/auth';
+import GitHubIcon from '@material-ui/icons/GitHub';
 
 function Login () {
 	const {login} = useContext(AuthContext);
 
 	return(
-		<div>
-			<button onClick={login}>Login</button>
-
-
-			<PokemonList />
-
-
+		<>
+		<div id="wrap_login">
+			<div className="indent">
+				<div className="titulo">
+					<h3>Login with:</h3>
+				</div>
+				<button onClick={login}><GitHubIcon/></button>
+			</div>
 		</div>
+			<PokemonList />
+		</>
 	)
 }
 
