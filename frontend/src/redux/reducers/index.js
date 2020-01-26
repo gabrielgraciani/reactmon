eec4181b802701f6ece1e6705bd8ef3c239d201a
Gabriel Graciani <gabrieltgraciani@hotmail.com>
2020-01-26 10:09:18 -0300

    tentando fazer o create pokemon com reducer

--- frontend/src/redux/reducers/index.js
+++ frontend/src/redux/reducers/index.js
@@ -1,4 +1,6 @@
 import {combineReducers} from 'redux';
+import pokemons from './pokemons';
 
 export default combineReducers({
+	pokemons
 })
---
+++ frontend/src/redux/reducers/pokemons/index.js
@@ -0,0 +1,21 @@
+import createReducer from '../create-reducer';
+import {ADD_POKEMON} from './actions';
+
+const initialState = {};
+
+const users = createReducer(initialState, {
+	[ADD_POKEMON]: (state, action) => ({
+		...state,
+		[action.payload.id]: {
+			id: action.payload.id,
+			nome: action.payload.nome,
+			tipo: action.payload.tipo,
+			altura: action.payload.altura,
+			peso: action.payload.peso,
+			fraquezas: action.payload.fraquezas,
+			evolucoes: action.payload.evolucoes
+		}
+	})
+});
+
+export default users;
