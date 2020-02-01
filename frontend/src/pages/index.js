36ec7b21d4c9db5c2bb6f1224634cc324e0abdae
Gabriel Graciani <gabrieltgraciani@hotmail.com>
2020-02-01 13:10:42 -0300

    finalização do card pokemon, junto com responsivo

--- frontend/src/pages/index.js
+++ frontend/src/pages/index.js
@@ -1,6 +1,5 @@
 import React, {useState, useEffect} from 'react'
 import axios from 'axios';
-import Background from 'assets/images/bg-item.png';
 
 function Home(){
 	const [data, setData] = useState({ pokemon: [] });
@@ -14,6 +13,7 @@ function Home(){
 		};
 		fetchData();
 	}, []);
+
 	return(
 		<>
 		<div id="wrap_banner">
@@ -26,7 +26,55 @@ function Home(){
 
 		</ul>
 
-		<div id="wrap_pokemons">
+		<div id="wrap_pokemon">
+			<div className="indent">
+				{data.pokemon.map(item => (
+					<div className={`card ${item.type[0].toLowerCase()}`} key={item.id}>
+						<div className="imagem">
+							<img src={item.img} className="pokemon" alt={item.name} />
+						</div>
+						<div className="conteudo">
+							<div className="tipos">
+								{item.type.map((tipo, index) => (
+									<div className={`tipo ${tipo.toLowerCase()}`} key={index}>
+										<span>{tipo}</span>
+									</div>
+								))}
+							</div>
+							<div className="nome">
+								<h4>{item.name}</h4>
+							</div>
+							<div className="especificacoes">
+								<div className="col">
+									<div className="item">
+										<span>Peso:</span>
+									</div>
+									<div className="item">
+										<span>Altura:</span>
+									</div>
+									<div className="item">
+										<span>Fraquezas:</span>
+									</div>
+								</div>
+								<div className="col col2">
+									<div className="item">
+										<span>55</span>
+									</div>
+									<div className="item">
+										<span>55</span>
+									</div>
+									<div className="item">
+										<span>55</span>
+									</div>
+								</div>
+							</div>
+						</div>
+					</div>
+				))}
+			</div>
+		</div>
+
+{/*		<div id="wrap_pokemons">
 			<div className="indent">
 				{data.pokemon.map(item => (
 					<div className="item" key={item.id}>
@@ -47,7 +95,7 @@ function Home(){
 					</div>
 				))}
 			</div>
-		</div>
+		</div>*/}
 		</>
 	)
 
