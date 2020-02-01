be35bbae15e8d401d5ecfc4529056707779d2e64
Gabriel Graciani <gabrieltgraciani@hotmail.com>
2020-02-01 11:43:05 -0300

    começo alteração cards pokemon

--- frontend/src/pages/pokedex.js
+++ frontend/src/pages/pokedex.js
@@ -1,7 +1,331 @@
 import React from 'react';
+import imagem from 'assets/images/venusaur.png';
+
 
 const Pokedex = () => (
-	<div>Pokedex</div>
+	<div id="wrap_pokemon2">
+		<div className="indent">
+			<div className="card normal">
+				<div className="imagem">
+					<img src={imagem} alt="teste"/>
+				</div>
+				<div className="conteudo">
+					<div className="tipos">
+						<div className="tipo normal">
+							<span>NORMAL</span>
+						</div>
+					</div>
+					<div className="nome">
+						<h4>Venusaur</h4>
+					</div>
+					<div className="especificacoes">
+						<div className="col">
+							<div className="item">
+								<span>Peso:</span>
+							</div>
+							<div className="item">
+								<span>Altura:</span>
+							</div>
+							<div className="item">
+								<span>Fraquezas:</span>
+							</div>
+						</div>
+						<div className="col col2">
+							<div className="item">
+								<span>55</span>
+							</div>
+							<div className="item">
+								<span>55</span>
+							</div>
+							<div className="item">
+								<span>55</span>
+							</div>
+						</div>
+					</div>
+				</div>
+			</div>
+
+			<div className="card water">
+				<div className="imagem">
+					<img src={imagem} alt="teste"/>
+				</div>
+				<div className="conteudo">
+					<div className="tipos">
+						<div className="tipo water">
+							<span>WATER</span>
+						</div>
+					</div>
+					<div className="nome">
+						<h4>Venusaur</h4>
+					</div>
+					<div className="especificacoes">
+						<div className="col">
+							<div className="item">
+								<span>Peso:</span>
+							</div>
+							<div className="item">
+								<span>Altura:</span>
+							</div>
+							<div className="item">
+								<span>Fraquezas:</span>
+							</div>
+						</div>
+						<div className="col col2">
+							<div className="item">
+								<span>55</span>
+							</div>
+							<div className="item">
+								<span>55</span>
+							</div>
+							<div className="item">
+								<span>55</span>
+							</div>
+						</div>
+					</div>
+				</div>
+			</div>
+
+			<div className="card electric">
+				<div className="imagem">
+					<img src={imagem} alt="teste"/>
+				</div>
+				<div className="conteudo">
+					<div className="tipos">
+						<div className="tipo electric">
+							<span>electric</span>
+						</div>
+					</div>
+					<div className="nome">
+						<h4>Venusaur</h4>
+					</div>
+					<div className="especificacoes">
+						<div className="col">
+							<div className="item">
+								<span>Peso:</span>
+							</div>
+							<div className="item">
+								<span>Altura:</span>
+							</div>
+							<div className="item">
+								<span>Fraquezas:</span>
+							</div>
+						</div>
+						<div className="col col2">
+							<div className="item">
+								<span>55</span>
+							</div>
+							<div className="item">
+								<span>55</span>
+							</div>
+							<div className="item">
+								<span>55</span>
+							</div>
+						</div>
+					</div>
+				</div>
+			</div>
+
+			<div className="card fire">
+				<div className="imagem">
+					<img src={imagem} alt="teste"/>
+				</div>
+				<div className="conteudo">
+					<div className="tipos">
+						<div className="tipo fire">
+							<span>fire</span>
+						</div>
+					</div>
+					<div className="nome">
+						<h4>Venusaur</h4>
+					</div>
+					<div className="especificacoes">
+						<div className="col">
+							<div className="item">
+								<span>Peso:</span>
+							</div>
+							<div className="item">
+								<span>Altura:</span>
+							</div>
+							<div className="item">
+								<span>Fraquezas:</span>
+							</div>
+						</div>
+						<div className="col col2">
+							<div className="item">
+								<span>55</span>
+							</div>
+							<div className="item">
+								<span>55</span>
+							</div>
+							<div className="item">
+								<span>55</span>
+							</div>
+						</div>
+					</div>
+				</div>
+			</div>
+
+			<div className="card psychic">
+				<div className="imagem">
+					<img src={imagem} alt="teste"/>
+				</div>
+				<div className="conteudo">
+					<div className="tipos">
+						<div className="tipo psychic">
+							<span>psychic</span>
+						</div>
+					</div>
+					<div className="nome">
+						<h4>Venusaur</h4>
+					</div>
+					<div className="especificacoes">
+						<div className="col">
+							<div className="item">
+								<span>Peso:</span>
+							</div>
+							<div className="item">
+								<span>Altura:</span>
+							</div>
+							<div className="item">
+								<span>Fraquezas:</span>
+							</div>
+						</div>
+						<div className="col col2">
+							<div className="item">
+								<span>55</span>
+							</div>
+							<div className="item">
+								<span>55</span>
+							</div>
+							<div className="item">
+								<span>55</span>
+							</div>
+						</div>
+					</div>
+				</div>
+			</div>
+
+			<div className="card grass">
+				<div className="imagem">
+					<img src={imagem} alt="teste"/>
+				</div>
+				<div className="conteudo">
+					<div className="tipos">
+						<div className="tipo grass">
+							<span>grass</span>
+						</div>
+					</div>
+					<div className="nome">
+						<h4>Venusaur</h4>
+					</div>
+					<div className="especificacoes">
+						<div className="col">
+							<div className="item">
+								<span>Peso:</span>
+							</div>
+							<div className="item">
+								<span>Altura:</span>
+							</div>
+							<div className="item">
+								<span>Fraquezas:</span>
+							</div>
+						</div>
+						<div className="col col2">
+							<div className="item">
+								<span>55</span>
+							</div>
+							<div className="item">
+								<span>55</span>
+							</div>
+							<div className="item">
+								<span>55</span>
+							</div>
+						</div>
+					</div>
+				</div>
+			</div>
+
+			<div className="card ice">
+				<div className="imagem">
+					<img src={imagem} alt="teste"/>
+				</div>
+				<div className="conteudo">
+					<div className="tipos">
+						<div className="tipo ice">
+							<span>ice</span>
+						</div>
+					</div>
+					<div className="nome">
+						<h4>Venusaur</h4>
+					</div>
+					<div className="especificacoes">
+						<div className="col">
+							<div className="item">
+								<span>Peso:</span>
+							</div>
+							<div className="item">
+								<span>Altura:</span>
+							</div>
+							<div className="item">
+								<span>Fraquezas:</span>
+							</div>
+						</div>
+						<div className="col col2">
+							<div className="item">
+								<span>55</span>
+							</div>
+							<div className="item">
+								<span>55</span>
+							</div>
+							<div className="item">
+								<span>55</span>
+							</div>
+						</div>
+					</div>
+				</div>
+			</div>
+
+			<div className="card fairy">
+				<div className="imagem">
+					<img src={imagem} alt="teste"/>
+				</div>
+				<div className="conteudo">
+					<div className="tipos">
+						<div className="tipo fairy">
+							<span>fairy</span>
+						</div>
+					</div>
+					<div className="nome">
+						<h4>Venusaur</h4>
+					</div>
+					<div className="especificacoes">
+						<div className="col">
+							<div className="item">
+								<span>Peso:</span>
+							</div>
+							<div className="item">
+								<span>Altura:</span>
+							</div>
+							<div className="item">
+								<span>Fraquezas:</span>
+							</div>
+						</div>
+						<div className="col col2">
+							<div className="item">
+								<span>55</span>
+							</div>
+							<div className="item">
+								<span>55</span>
+							</div>
+							<div className="item">
+								<span>55</span>
+							</div>
+						</div>
+					</div>
+				</div>
+			</div>
+		</div>
+	</div>
 );
 
 export default Pokedex;
