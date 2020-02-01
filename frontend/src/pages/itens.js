c51a49a96c6c3c7bc295bd06436d25fdcb8f3ee9
Gabriel Graciani <gabrieltgraciani@hotmail.com>
2020-02-01 20:06:03 -0300

    desenvolvimento cards itens

--- frontend/src/pages/itens.js
+++ frontend/src/pages/itens.js
@@ -1,7 +1,77 @@
 import React from 'react';
 
 const Itens = () => (
-	<div>Itens</div>
+	<div id="wrap_itens">
+		<div className="indent">
+			<div className="item">
+				<div className="imagem">
+					<img src="https://pokemythology.net/conteudo/imgs/dw/item/Dream_Potion_Sprite.png" alt="pocao" />
+				</div>
+				<div className="conteudo">
+					<div className="nome">
+						<h4>Poção</h4>
+					</div>
+					<div className="descricao">
+						<span>Poção utilizada tanto em batalhas pokémon ou em capturas</span>
+					</div>
+				</div>
+				<div className="footer">
+					<h4>Recupera 20 HP</h4>
+				</div>
+			</div>
+
+			<div className="item">
+				<div className="imagem">
+					<img src="https://pokemythology.net/conteudo/imgs/dw/item/Dream_Super_Potion_Sprite.png" alt="pocao" />
+				</div>
+				<div className="conteudo">
+					<div className="nome">
+						<h4>Super Poção</h4>
+					</div>
+					<div className="descricao">
+						<span>Poção utilizada tanto em batalhas pokémon ou em capturas</span>
+					</div>
+				</div>
+				<div className="footer">
+					<h4>Recupera 50 HP</h4>
+				</div>
+			</div>
+
+			<div className="item">
+				<div className="imagem">
+					<img src="https://pokemythology.net/conteudo/imgs/dw/item/Dream_Hyper_Potion_Sprite.png" alt="pocao" />
+				</div>
+				<div className="conteudo">
+					<div className="nome">
+						<h4>Hyper Poção</h4>
+					</div>
+					<div className="descricao">
+						<span>Poção utilizada tanto em batalhas pokémon ou em capturas</span>
+					</div>
+				</div>
+				<div className="footer">
+					<h4>Recupera 200 HP</h4>
+				</div>
+			</div>
+
+			<div className="item">
+				<div className="imagem">
+					<img src="https://pokemythology.net/conteudo/imgs/dw/item/Dream_Max_Potion_Sprite.png" alt="pocao" />
+				</div>
+				<div className="conteudo">
+					<div className="nome">
+						<h4>Max Potion</h4>
+					</div>
+					<div className="descricao">
+						<span>Poção utilizada tanto em batalhas pokémon ou em capturas</span>
+					</div>
+				</div>
+				<div className="footer">
+					<h4>Recupera todo o HP</h4>
+				</div>
+			</div>
+		</div>
+	</div>
 );
 
 export default Itens;
