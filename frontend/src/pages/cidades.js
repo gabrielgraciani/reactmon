cdbc07a0fae54b3ccc757e4b896953e19bbcd4d3
Gabriel Graciani <gabrieltgraciani@hotmail.com>
2020-02-01 21:00:26 -0300

    finalizado card itens e desenvolvimento card cidades

--- frontend/src/pages/cidades.js
+++ frontend/src/pages/cidades.js
@@ -1,7 +1,89 @@
 import React from 'react';
 
 const Cidades = () => (
-	<div>Cidades</div>
+	<div id="wrap_cidades">
+		<div className="indent">
+			<div className="cidade">
+				<div className="imagem">
+					<img src="https://encrypted-tbn0.gstatic.com/images?q=tbn%3AANd9GcSCesKFa9Bj3NHo8TZJZGm3Ml-a8GoS4WT2slAe18nNrm3QTJ0z" alt="cidade"/>
+				</div>
+				<svg class="card__svg" viewBox="0 0 800 500">
+					<path d="M 0 100 Q 50 200 100 250 Q 250 400 350 300 C 400 250 550 150 650 300 Q 750 450 800 400 L 800 500 L 0 500" stroke="transparent" fill="#333"></path>
+					<path class="card__line" d="M 0 100 Q 50 200 100 250 Q 250 400 350 300 C 400 250 550 150 650 300 Q 750 450 800 400" stroke="pink" stroke-width="3" fill="transparent"></path>
+				</svg>
+				<div className="conteudo">
+					<div className="nome">
+						<h4>Lorem ipsum</h4>
+					</div>
+					<div className="descricao">
+						<span>Lorem ipsum dolor sit amet consectetur adipisicing elit. Soluta dolor
+							praesentium at quod autem omnis, amet eaque unde perspiciatis adipisci possimus
+							quam facere illo et quisquam quia earum nesciunt porro.</span>
+					</div>
+				</div>
+			</div>
+
+			<div className="cidade">
+				<div className="imagem">
+					<img src="https://encrypted-tbn0.gstatic.com/images?q=tbn%3AANd9GcSCesKFa9Bj3NHo8TZJZGm3Ml-a8GoS4WT2slAe18nNrm3QTJ0z" alt="cidade"/>
+				</div>
+				<svg class="card__svg" viewBox="0 0 800 500">
+					<path d="M 0 100 Q 50 200 100 250 Q 250 400 350 300 C 400 250 550 150 650 300 Q 750 450 800 400 L 800 500 L 0 500" stroke="transparent" fill="#333"></path>
+					<path class="card__line" d="M 0 100 Q 50 200 100 250 Q 250 400 350 300 C 400 250 550 150 650 300 Q 750 450 800 400" stroke="pink" stroke-width="3" fill="transparent"></path>
+				</svg>
+				<div className="conteudo">
+					<div className="nome">
+						<h4>Lorem ipsum</h4>
+					</div>
+					<div className="descricao">
+						<span>Lorem ipsum dolor sit amet consectetur adipisicing elit. Soluta dolor
+							praesentium at quod autem omnis, amet eaque unde perspiciatis adipisci possimus
+							quam facere illo et quisquam quia earum nesciunt porro.</span>
+					</div>
+				</div>
+			</div>
+
+			<div className="cidade">
+				<div className="imagem">
+					<img src="https://encrypted-tbn0.gstatic.com/images?q=tbn%3AANd9GcSCesKFa9Bj3NHo8TZJZGm3Ml-a8GoS4WT2slAe18nNrm3QTJ0z" alt="cidade"/>
+				</div>
+				<svg class="card__svg" viewBox="0 0 800 500">
+					<path d="M 0 100 Q 50 200 100 250 Q 250 400 350 300 C 400 250 550 150 650 300 Q 750 450 800 400 L 800 500 L 0 500" stroke="transparent" fill="#333"></path>
+					<path class="card__line" d="M 0 100 Q 50 200 100 250 Q 250 400 350 300 C 400 250 550 150 650 300 Q 750 450 800 400" stroke="pink" stroke-width="3" fill="transparent"></path>
+				</svg>
+				<div className="conteudo">
+					<div className="nome">
+						<h4>Lorem ipsum</h4>
+					</div>
+					<div className="descricao">
+						<span>Lorem ipsum dolor sit amet consectetur adipisicing elit. Soluta dolor
+							praesentium at quod autem omnis, amet eaque unde perspiciatis adipisci possimus
+							quam facere illo et quisquam quia earum nesciunt porro.</span>
+					</div>
+				</div>
+			</div>
+
+			<div className="cidade">
+				<div className="imagem">
+					<img src="https://encrypted-tbn0.gstatic.com/images?q=tbn%3AANd9GcSCesKFa9Bj3NHo8TZJZGm3Ml-a8GoS4WT2slAe18nNrm3QTJ0z" alt="cidade"/>
+				</div>
+				<svg class="card__svg" viewBox="0 0 800 500">
+					<path d="M 0 100 Q 50 200 100 250 Q 250 400 350 300 C 400 250 550 150 650 300 Q 750 450 800 400 L 800 500 L 0 500" stroke="transparent" fill="#333"></path>
+					<path class="card__line" d="M 0 100 Q 50 200 100 250 Q 250 400 350 300 C 400 250 550 150 650 300 Q 750 450 800 400" stroke="pink" stroke-width="3" fill="transparent"></path>
+				</svg>
+				<div className="conteudo">
+					<div className="nome">
+						<h4>Lorem ipsum</h4>
+					</div>
+					<div className="descricao">
+						<span>Lorem ipsum dolor sit amet consectetur adipisicing elit. Soluta dolor
+							praesentium at quod autem omnis, amet eaque unde perspiciatis adipisci possimus
+							quam facere illo et quisquam quia earum nesciunt porro.</span>
+					</div>
+				</div>
+			</div>
+		</div>
+	</div>
 );
 
 export default Cidades;
