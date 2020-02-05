ab6b839154c9ba684b9c37c3ed558f5e321a15ea
Gabriel Graciani <gabrieltgraciani@hotmail.com>
2020-02-04 21:59:44 -0300

    criação dos arquivos de redux para o crud item

--- frontend/src/pages/crud-item.js
+++ frontend/src/pages/crud-item.js
@@ -1,7 +1,20 @@
 import React from 'react';
 
-const Crud_Item = () => (
-	<div>item</div>
-);
+function Crud_Item(){
+
+	const onSubmit = (e) => {
+		e.preventDefault();
+		const {nome, descricao} = e.target;
+		console.log(nome.value, descricao.value);
+	}
+
+	return(
+		<form action="" onSubmit={onSubmit}>
+			<input type="text" name="nome" placeholder="Nome" />
+			<input type="text" name="descricao" placeholder="Descrição"/>
+			<input type="submit" value="Enviar"/>
+		</form>
+	)
+}
 
 export default Crud_Item;
--- frontend/src/redux/rootSaga.js
+++ frontend/src/redux/rootSaga.js
@@ -1,11 +1,9 @@
 import { all } from 'redux-saga/effects';
 
 import pokemonSaga from './sagas/pokemon';
-import itemSaga from './sagas/item';
 
 export default function* rootSaga() {
 	yield all([
-		pokemonSaga(),
-		itemSaga()
+		pokemonSaga()
 	]);
 }
