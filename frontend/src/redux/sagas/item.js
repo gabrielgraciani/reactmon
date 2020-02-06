06c3cee975b42aa533c4f6761dcc95e71239453a
Gabriel Thomaz Graciani <gabrieltgraciani@hotmail.com>
2020-02-06 12:44:07 -0300

    feito cadastro no firebase com redux saga

--- frontend/src/redux/sagas/item.js
+++ frontend/src/redux/sagas/item.js
@@ -1,9 +1,24 @@
 import { takeLatest, all, call, put } from 'redux-saga/effects';
+import {db} from 'services/firebase';
 
 import * as actions from '../actions/item';
 
-function* sendItemWorker() {
+function* sendItemWorker(data) {
 	try {
+		const {nome, descricao} = data.payload;
+		console.log(nome, descricao);
+
+		db.collection('item').add({
+		id: Math.random(),
+		nome,
+		descricao
+		})
+		.then(function(docRef){
+			db.collection('item').doc(docRef.id).update({
+				id: docRef.id
+			});
+		})
+
 
 	} catch (error) {
 		console.log('error', error);
