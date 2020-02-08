67250ef110e785c7679de7f7599bb906258208df
Gabriel Graciani <gabrieltgraciani@hotmail.com>
2020-02-08 10:03:47 -0300

    feito listar itens na tabela

--- frontend/src/components/crud/item/dataList.js
+++ frontend/src/components/crud/item/dataList.js
@@ -1,27 +1,45 @@
-import React from 'react';
+import React, {useEffect} from 'react';
+import {useDispatch, useSelector} from "react-redux";
+import {itemFetch} from "../../../redux/actions/item";
 import EditIcon from '@material-ui/icons/Edit';
 import DeleteIcon from '@material-ui/icons/Delete';
 import CircularProgress from '@material-ui/core/CircularProgress';
 
-function dataList(){
+
+function DataList(){
+	const dispatch = useDispatch();
+
+	const { list, isLoading } = useSelector(store => store.item);
+	console.log("list", list);
+
+	useEffect(() => {
+		if(list.length === 0){
+			dispatch(itemFetch());
+		}
+	}, [isLoading]);
 
 	return(
 		<>
 		<tbody>
-			<tr className="row">
-				<td className="item">id</td>
-				<td className="item">nome</td>
-				<td className="item">descricao</td>
-
+		{isLoading && (
+			<div className="loading">
+				<CircularProgress size={50} />
+			</div>
+		)}
+		{list.map((item, index) => (
+			<tr className="row" key={item.id}>
+				<td className="item">{item.id}</td>
+				<td className="item">{item.nome}</td>
+				<td className="item">{item.descricao}</td>
 				<td className="item actions">
 					<div className="icon"><EditIcon className="edit" /></div>
 					<div className="icon"><DeleteIcon className="delete" /></div>
 				</td>
 			</tr>
+		))}
 		</tbody>
-
 		</>
 	)
 }
 
-export default dataList;
+export default DataList;
--- frontend/src/pages/crud-item.js
+++ frontend/src/pages/crud-item.js
@@ -1,22 +1,12 @@
 import React, {useEffect} from 'react';
-import {useDispatch, useSelector} from "react-redux";
-import {itemSend, itemFetch} from "../redux/actions/item";
-import CircularProgress from '@material-ui/core/CircularProgress';
+import {useDispatch} from "react-redux";
+import {itemSend} from "../redux/actions/item";
 import Form from 'components/crud/item/formulario';
 import Tabela from 'components/crud/item/tabela';
 
 
 function Crud_Item(){
 	const dispatch = useDispatch();
-
-	const { list, isLoading } = useSelector(store => store.item);
-	console.log("list", list);
-
-	useEffect(() => {
-		if(list.length === 0){
-			dispatch(itemFetch());
-		}
-	}, [isLoading]);
 
 	const onSubmit = (e) => {
 		e.preventDefault();
@@ -49,13 +39,6 @@ function Crud_Item(){
 			<input type="text" name="descricao" placeholder="Descrição"/>
 			<input type="submit" value="Enviar"/>
 		</form>
-
-		{isLoading && (
-			<CircularProgress size={25} />
-		)}
-		{list.map((item, index) => (
-				<div key={index}>{item.nome}</div>
-			))}
 		</>
 
 
