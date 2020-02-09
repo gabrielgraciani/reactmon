013d86ef8a04d9e59571eb712bae23e0f69a5770
Gabriel Graciani <gabrieltgraciani@hotmail.com>
2020-02-09 18:28:27 -0300

    link footer

--- frontend/src/components/footer.js
+++ frontend/src/components/footer.js
@@ -4,7 +4,7 @@ const Footer = () => (
 	<div id="wrap_footer">
 		<div className="indent">
 			<div className="texto">
-				<span>&copy; Desenvolvido por Gabriel Thomaz Graciani</span>
+				<span>&copy; Desenvolvido por <a href="https://www.linkedin.com/in/gabriel-thomaz-graciani-98400b166/" target="_blank">Gabriel Thomaz Graciani</a></span>
 			</div>
 		</div>
 	</div>
