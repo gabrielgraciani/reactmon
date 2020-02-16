7458371397dee2b37b449e3342357fd72aa21375
Gabriel Graciani <gabrieltgraciani@hotmail.com>
2020-02-16 09:59:33 -0300

    começo desenvolvimento login

--- frontend/src/components/auth/index.js
+++ frontend/src/components/auth/index.js
@@ -1,11 +1,27 @@
-import React from 'react';
+import React, {useState} from 'react';
 import EmailIcon from '@material-ui/icons/Email';
 import LockIcon from '@material-ui/icons/Lock';
 import {authOpenForm} from '../../redux/actions/auth';
 import {useDispatch} from "react-redux";
 
 function LoginForm () {
+	const initialState = {
+		email: '',
+		senha: ''
+	};
+	const [values, setValues] = useState(initialState);
+
 	const dispatch = useDispatch();
+
+	const handleChange = (e) => setValues({
+		...values,
+		[e.target.name]: e.target.value
+	});
+
+	const handleSubmit = (e) => {
+		e.preventDefault();
+		console.log(values);
+	};
 
 	return(
 		<div id="wrap_login">
@@ -16,15 +32,15 @@ function LoginForm () {
 				<form action="">
 					<div className="item">
 						<EmailIcon />
-						<input type="text" placeholder="E-mail" />
+						<input type="text" placeholder="E-mail" name="email" value={values.email} onChange={handleChange} autoComplete="off" />
 					</div>
 
 					<div className="item">
 						<LockIcon />
-						<input type="password" placeholder="Senha"/>
+						<input type="password" placeholder="Senha" name="senha" value={values.senha} onChange={handleChange} autoComplete="off" />
 					</div>
 
-					<input type="submit" value="LOGIN" />
+					<input type="submit" value="LOGIN" onClick={handleSubmit} />
 				</form>
 
 				<div className="criar">
