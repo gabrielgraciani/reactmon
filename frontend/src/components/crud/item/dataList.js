47d11c984a59ac7b4d32940c07f7a856fa22ae77
Gabriel Graciani <gabrieltgraciani@hotmail.com>
2020-02-18 21:02:17 -0300

    feito sistema de busca sem redux, refatorar para colocar em redux depois

--- frontend/src/components/crud/item/dataList.js
+++ frontend/src/components/crud/item/dataList.js
@@ -1,10 +1,12 @@
-import React, {useEffect} from 'react';
+import React, {useEffect, useState} from 'react';
 import DeleteIcon from '@material-ui/icons/Delete';
 import EditIcon from '@material-ui/icons/Edit';
 import GifPikachu from 'assets/images/gifs/gif-pikachu.gif';
 import Loading from 'components/loading';
 import {itemDelete, itemFetch, itemShowEdit} from "../../../redux/actions/item";
 import {useDispatch, useSelector} from "react-redux";
+import {db} from 'services/firebase';
+import SearchIcon from '@material-ui/icons/Search';
 
 function DataList(){
 	const dispatch = useDispatch();
@@ -28,9 +30,53 @@ function DataList(){
 		return () => window.removeEventListener('scroll', handleScroll);
 	}, [isLoading, dispatch, last]);
 
+
+	const [item, setItem] = useState([]);
+	const [searchTerm, setSearchTerm] = useState('');
+	const [searchResult, setSearchResult] = useState([]);
+
+	useEffect(() => {
+		let itemList = [];
+		db.collection('item').get().then(querySnapshot => {
+			querySnapshot.forEach(doc => {
+				itemList.push({
+					id: doc.id,
+					...doc.data()
+				})
+			});
+			setItem(itemList);
+		});
+
+		const results = item.filter(item =>
+		item.nome.includes(searchTerm));
+		setSearchResult(results);
+		console.log('results', results);
+
+	}, [item, searchTerm]);
+	console.log('item', item);
+
+	const handleChange = (e) => {
+		setSearchTerm(e.target.value);
+		console.log(e.target.value);
+	};
+
 	return(
 		<>
-		{list.map((item, index) => (
+
+		<div className="search">
+			<input className="search-input" type="text" name="" value={searchTerm} onChange={handleChange} placeholder="digita" />
+			<div className="search-btn">
+				<SearchIcon />
+			</div>
+		</div>
+
+
+
+		{searchTerm && searchResult.map((item) => (
+			<div key={item.id}>{item.nome}</div>
+		))}
+
+		{!searchTerm && list.map((item, index) => (
 			<div className="row" key={item.id}>
 				<div className="item">{item.id}</div>
 				<div className="item">{item.nome}</div>
