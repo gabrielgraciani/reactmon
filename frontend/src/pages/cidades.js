75dc636e76dcdac0f29b1b87ccafaf0971480c6b
Gabriel Graciani <gabrieltgraciani@hotmail.com>
2020-02-22 09:35:41 -0300

    começo pagina cidade

--- frontend/src/pages/cidades.js
+++ frontend/src/pages/cidades.js
@@ -1,9 +1,65 @@
-import React from 'react';
+import React, {useEffect, useState} from 'react';
 import Banner from 'components/banner';
+import {cidadeFetch, cidadeFetchSearch} from "../redux/actions/cidade";
+import {useDispatch, useSelector} from "react-redux";
+import Loading from 'components/loading';
+import GifSquirtle from 'assets/images/gifs/gif-squirtle.gif';
 import Search from 'components/search';
 
-const Cidades = () => (
-	<>
+function Cidades(){
+
+	const [searchTerm, setSearchTerm] = useState('');
+	const [searchResult, setSearchResult] = useState([]);
+	const [active, setActive] = useState('');
+
+	const dispatch = useDispatch();
+	const { listSearch, endInfiniteScroll, isLoading, last, list = [] } = useSelector(store => store.cidade);
+
+	const handleChange = (e) => {
+		setSearchTerm(e.target.value.toLowerCase());
+	};
+
+	useEffect(() => {
+		if(listSearch.length === 0){
+			dispatch(cidadeFetchSearch());
+		}
+	}, [dispatch, listSearch.length]);
+
+	useEffect(() => {
+		if(list.length === 0){
+			dispatch(cidadeFetch());
+		}
+	}, [dispatch, list.length]);
+
+	useEffect(() => {
+		if(!searchTerm){
+			function handleScroll() {
+				if (Math.round(window.innerHeight + document.documentElement.scrollTop) === document.documentElement.offsetHeight ||
+					Math.round(window.innerHeight + document.documentElement.scrollTop + 1) >= document.documentElement.offsetHeight){
+					if(last){ dispatch(cidadeFetch()); }
+				}
+			}
+
+			window.addEventListener('scroll', handleScroll);
+			return () => window.removeEventListener('scroll', handleScroll);
+		}
+	}, [searchTerm, isLoading, dispatch, last]);
+
+	useEffect(() => {
+		const results = listSearch.filter(cidade =>
+			cidade.nome.toLowerCase().includes(searchTerm));
+		setSearchResult(results);
+
+		if(searchTerm){
+			setActive('active');
+		} else{
+			setActive('');
+		}
+
+	}, [listSearch, searchTerm]);
+
+	return(
+		<>
 		<Banner className="cidades" titulo="Cidades">
 			<p>Muitas regiões foram descritas na franquia de jogos eletrônicos, desenhos animados e
 				quadrinhos Pokémon. Cada uma das gerações de RPGs Originais de Pokémon introduziu uma nova Região.
@@ -19,93 +75,75 @@ const Cidades = () => (
 				e Pokémon Silver, o mapa refere-se a Kanto e Johto como countrys.</p>
 		</Banner>
 
-		<Search placeholder="Pesquise uma cidade" />
+		<Search active={active} value={searchTerm} handleChange={handleChange} placeholder="Pesquise uma cidade" />
 
 		<div id="wrap_cidades">
 			<div className="indent">
-				<div className="cidade">
-					<div className="imagem">
-						<img src="https://encrypted-tbn0.gstatic.com/images?q=tbn%3AANd9GcSCesKFa9Bj3NHo8TZJZGm3Ml-a8GoS4WT2slAe18nNrm3QTJ0z" alt="cidade"/>
+				{searchTerm && searchResult.map((cidade) => (
+					<div className="cidade">
+						<div className="imagem">
+							<img src={cidade.imagem.url} alt="cidade"/>
 
-						<svg class="wavy" viewBox="0 0 500 500" preserveAspectRatio="xMinYMin meet">
-							<path d="M0,100 C150,200 350,0 500,100 L500,00 L0,0 Z" style={{stroke: 'none'}}></path>
-						</svg>
-					</div>
-					<div className="conteudo">
-						<div className="nome">
-							<h4>Lorem ipsum</h4>
+							<svg class="wavy" viewBox="0 0 500 500" preserveAspectRatio="xMinYMin meet">
+								<path d="M0,100 C150,200 350,0 500,100 L500,00 L0,0 Z" style={{stroke: 'none'}}></path>
+							</svg>
 						</div>
-						<div className="descricao">
-							<span>Lorem ipsum dolor sit amet consectetur adipisicing elit. Soluta dolor
-								praesentium at quod autem omnis, amet eaque unde perspiciatis adipisci possimus
-								quam facere illo et quisquam quia earum nesciunt porro.</span>
-						</div>
-					</div>
-				</div>
-
-				<div className="cidade">
-					<div className="imagem">
-						<img src="https://encrypted-tbn0.gstatic.com/images?q=tbn%3AANd9GcSCesKFa9Bj3NHo8TZJZGm3Ml-a8GoS4WT2slAe18nNrm3QTJ0z" alt="cidade"/>
-
-						<svg class="wavy" viewBox="0 0 500 500" preserveAspectRatio="xMinYMin meet">
-							<path d="M0,100 C150,200 350,0 500,100 L500,00 L0,0 Z" style={{stroke: 'none'}}></path>
-						</svg>
-					</div>
-					<div className="conteudo">
-						<div className="nome">
-							<h4>Lorem ipsum</h4>
-						</div>
-						<div className="descricao">
-							<span>Lorem ipsum dolor sit amet consectetur adipisicing elit. Soluta dolor
-								praesentium at quod autem omnis, amet eaque unde perspiciatis adipisci possimus
-								quam facere illo et quisquam quia earum nesciunt porro.</span>
+						<div className="conteudo">
+							<div className="nome">
+								<h4>{cidade.nome}</h4>
+							</div>
+							<div className="descricao">
+								<span>{cidade.descricao}</span>
+							</div>
 						</div>
 					</div>
-				</div>
+				))}
 
-				<div className="cidade">
-					<div className="imagem">
-						<img src="https://encrypted-tbn0.gstatic.com/images?q=tbn%3AANd9GcSCesKFa9Bj3NHo8TZJZGm3Ml-a8GoS4WT2slAe18nNrm3QTJ0z" alt="cidade"/>
+				{!searchTerm && list.map((cidade) => (
+					<div className="cidade">
+						<div className="imagem">
+							<img src={cidade.imagem.url} alt="cidade"/>
 
-						<svg class="wavy" viewBox="0 0 500 500" preserveAspectRatio="xMinYMin meet">
-							<path d="M0,100 C150,200 350,0 500,100 L500,00 L0,0 Z" style={{stroke: 'none'}}></path>
-						</svg>
-					</div>
-					<div className="conteudo">
-						<div className="nome">
-							<h4>Lorem ipsum</h4>
+							<svg class="wavy" viewBox="0 0 500 500" preserveAspectRatio="xMinYMin meet">
+								<path d="M0,100 C150,200 350,0 500,100 L500,00 L0,0 Z" style={{stroke: 'none'}}></path>
+							</svg>
 						</div>
-						<div className="descricao">
-							<span>Lorem ipsum dolor sit amet consectetur adipisicing elit. Soluta dolor
-								praesentium at quod autem omnis, amet eaque unde perspiciatis adipisci possimus
-								quam facere illo et quisquam quia earum nesciunt porro.</span>
-						</div>
-					</div>
-				</div>
-
-				<div className="cidade">
-					<div className="imagem">
-						<img src="https://encrypted-tbn0.gstatic.com/images?q=tbn%3AANd9GcSCesKFa9Bj3NHo8TZJZGm3Ml-a8GoS4WT2slAe18nNrm3QTJ0z" alt="cidade"/>
-
-						<svg class="wavy" viewBox="0 0 500 500" preserveAspectRatio="xMinYMin meet">
-							<path d="M0,100 C150,200 350,0 500,100 L500,00 L0,0 Z" style={{stroke: 'none'}}></path>
-						</svg>
-					</div>
-					<div className="conteudo">
-						<div className="nome">
-							<h4>Lorem ipsum</h4>
-						</div>
-						<div className="descricao">
-							<span>Lorem ipsum dolor sit amet consectetur adipisicing elit. Soluta dolor
-								praesentium at quod autem omnis, amet eaque unde perspiciatis adipisci possimus
-								quam facere illo et quisquam quia earum nesciunt porro.</span>
+						<div className="conteudo">
+							<div className="nome">
+								<h4>{cidade.nome}</h4>
+							</div>
+							<div className="descricao">
+								<span>{cidade.descricao}</span>
+							</div>
 						</div>
 					</div>
-				</div>
+				))}
 
+				{!searchTerm && endInfiniteScroll && (
+					<div className="cidade fim">
+						<div className="imagem">
+							<img src={GifSquirtle} alt="Squirtle chorando" />
+							<svg class="wavy" viewBox="0 0 500 500" preserveAspectRatio="xMinYMin meet">
+								<path d="M0,100 C150,200 350,0 500,100 L500,00 L0,0 Z" style={{stroke: 'none'}}></path>
+							</svg>
+						</div>
+						<div className="conteudo">
+							<div className="nome">
+								<h4>Não há mais registros abaixo... =(</h4>
+							</div>
+						</div>
+					</div>
+				)}
 			</div>
+			{!searchTerm && isLoading && (
+				<div className="loading">
+					<Loading />
+				</div>
+			)}
 		</div>
-	</>
-);
+		</>
+	)
+}
+
 
 export default Cidades;
