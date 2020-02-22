c6a92b3b5e9e32980ea4714a66e9a0a00aed4c02
Gabriel Graciani <gabrieltgraciani@hotmail.com>
2020-02-22 17:04:45 -0300

    desenvolvimento da página home

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,8 +1,9 @@
 import React, {useEffect} from 'react'
-import Carousel from 'components/carousel';
 import {pokemonFetch} from "../redux/actions/pokemon";
 import {useDispatch, useSelector} from "react-redux";
 import Loading from 'components/loading';
+import Banner from 'components/banner';
+import {Link} from 'react-router-dom';
 
 function Home(){
 
@@ -18,9 +19,16 @@ function Home(){
 
 	return(
 		<>
-		<div id="wrap_banner">
-			<Carousel />
-		</div>
+		<Banner className="home" titulo="História">
+			<p>É uma franquia de mídia que pertence a The Pokémon Company, tendo sido criada por Satoshi Tajiri em 1995. Ela é centrada
+				em criaturas ficcionais chamadas "Pokémon", que os seres humanos capturam e os treinam para lutarem um contra o
+				outro como um esporte.</p>
+
+			<p>A franquia começou com um par de jogos lançados para o Game Boy original, desenvolvidos pela Game Freak
+				e publicados pela Nintendo. Atualmente, a franquia se estende em jogos, cartas colecionáveis, série de televisão,
+				além de filmes, mangás e brinquedos. Pokémon é a segunda franquia de mídia de jogos mais bem sucedida e lucrativa do mundo,
+				atrás da franquia de Mario que também pertence a Nintendo.</p>
+		</Banner>
 
 		<div id="wrap_pokemon">
 			{isLoading && (
@@ -28,6 +36,11 @@ function Home(){
 					<Loading />
 				</div>
 			)}
+			<div className="vermais">
+				<Link to={'/'}>
+					<li>Home</li>
+				</Link>
+			</div>
 			<div className="indent">
 				{list.map((item) => (
 					<div className={`card ${item.tipo[0].toLowerCase()}`} key={item.id}>
